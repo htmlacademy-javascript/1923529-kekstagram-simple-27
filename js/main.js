9e0b3f7ec9ea3669330aacb9696a6bc1500b41e9
Павел <pasha.seregin.92@mail.ru>
2022-10-08 17:24:08 +0300

    Рефакторинг

--- js/main.js
+++ js/main.js
@@ -29,29 +29,35 @@ const getRandomInteger = (a, b) => {
   if (typeof a !== 'number') {
     throw 'Параметр "a" не является числовым значением';
   }
+
   if (typeof b !== 'number') {
     throw 'Параметр "b" не является числовым значением';
   }
+
   if (a < 0) {
     throw 'Параметр "a" не может быть отрицательным числом';
   }
+
   if (b < 0) {
     throw 'Параметр "b "не может быть отрицательным числом';
   }
+
   if (!Number.isFinite(a)) {
     throw 'Параметр "a" не является конечным значением';
   }
+
   if (!Number.isFinite(b)) {
     throw 'Параметр "b" не является конечным значением';
   }
-  if (a === b) {
-    return a;
+
+  if (Math.abs(b - a) < 1) {
+    throw 'Нет целых чисел в диапазоне';
   }
 
-  const min = Math.floor(Math.min(a, b));
+  const min = Math.ceil(Math.min(a, b));
   const max = Math.floor(Math.max(a, b));
 
-  if (min >= 0 && max > min) {
+  if (min >= 0) {
     return Math.floor(Math.random() * (max - min + 1) + min);
   }
 
@@ -61,9 +67,18 @@ const getRandomInteger = (a, b) => {
 export const checkingStringLength = (string, length) => string.length <= length;
 
 const getRandomArrayElement = (elements) => {
-  if (elements.length === 0) {
-    return NaN;
+  if (!Array.isArray(elements)) {
+    return undefined;
   }
+
+  if (elements.length === 0) {
+    return undefined;
+  }
+
+  if (elements.length === 1) {
+    return elements[0];
+  }
+
   return elements[getRandomInteger(0, elements.length - 1)];
 };
 
@@ -77,7 +92,8 @@ const makePhotoDescription = (i) => ({
 
 export const generationPhotos = () => {
   const photos = [];
-  for (let i = 0; i <= 24; i++) {
+
+  for (let i = 0; i < 25; i++) {
     photos.push(makePhotoDescription(i));
   }
 
